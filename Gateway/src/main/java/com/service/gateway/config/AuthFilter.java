184167564661f5763d15a58ebfaadc80a1486f69
juan jose <gestion@starplast.es>
2024-05-09 10:53:44 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/AuthFilter.java
+++ Gateway/src/main/java/com/service/gateway/config/AuthFilter.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.service.gateway.dto.TokenDto;
@@ -34,10 +36,6 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config>{
 		return (((exchange, chain)->{
 			String tokenHeader = null;
 			TokenDto tokenDto = null;
-			if(exchange.getRequest().getPath().toString().equals("/app/login"))
-				return chain.filter(exchange);
-			if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-				return onError(exchange, HttpStatus.BAD_REQUEST);}
 			if(exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
 				tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
 				String [] chunks = tokenHeader.split(" ");
@@ -46,21 +44,22 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config>{
 				String token = chunks[1];
 				tokenDto = TokenDto.builder().token(token).build();
 			}
-			
-			if (tokenDto == null) {
-	            return onError(exchange, HttpStatus.UNAUTHORIZED);
-	        }
-			
 			return webClient.build()
 					.post()
 					.uri("http://auth-service/auth/validate")
 					.bodyValue(tokenDto)
 					.retrieve()
 					.bodyToMono(TokenDto.class)
-					.map(t -> {
-						t.getToken();
-						return exchange;
-					}).flatMap(chain::filter);
+					.flatMap(t -> chain.filter(exchange))
+					.onErrorResume(error -> {
+	                    if (error instanceof WebClientResponseException) {
+	                        HttpStatusCode statusCode = ((WebClientResponseException) error).getStatusCode();
+	                        if (statusCode == HttpStatus.UNAUTHORIZED || statusCode == HttpStatus.BAD_REQUEST) {
+	                            return onError(exchange, HttpStatus.TEMPORARY_REDIRECT);
+	                        }
+	                    }
+	                    return Mono.error(error);
+	                });
 		}));
 	}
 	
