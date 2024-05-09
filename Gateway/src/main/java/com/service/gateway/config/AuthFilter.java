1dfdf577161ddc9d4dc71a7af78011ee5e1a2b1c
Juan José López <78323736+juanjolt97@users.noreply.github.com>
2024-05-09 07:44:02 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/AuthFilter.java
+++ Gateway/src/main/java/com/service/gateway/config/AuthFilter.java
@@ -32,6 +32,8 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config>{
 	@Override
 	public GatewayFilter apply(Config config) {
 		return (((exchange, chain)->{
+			if(exchange.getRequest().getPath().toString().equals("/app/login"))
+				return chain.filter(exchange);
 			String tokenHeader = null;
 			TokenDto tokenDto = null;
 			if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
