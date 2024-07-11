e08341913bc9d27e173a6009cc0947fe1b66cdf0
juan jose <gestion@starplast.es>
2024-07-11 19:13:34 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -12,6 +12,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class WebClientConfig {
@@ -19,16 +20,16 @@ public class WebClientConfig {
 	@Bean
 	@LoadBalanced
 	public WebClient.Builder builder(){
+		ConnectionProvider provider = ConnectionProvider.builder("fixed")
+			    .maxConnections(500)  
+			    .maxIdleTime(Duration.ofSeconds(20))  
+			    .maxLifeTime(Duration.ofSeconds(60)) 
+			    .pendingAcquireTimeout(Duration.ofSeconds(60))  
+			    .evictInBackground(Duration.ofSeconds(120)) 
+			    .build();
 		return WebClient.builder()
 				.clientConnector(new ReactorClientHttpConnector(
-			            HttpClient.create()
-			                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-			                .responseTimeout(Duration.ofSeconds(5))
-			                .doOnConnected(conn -> 
-			                    conn.addHandlerLast(new ReadTimeoutHandler(5))
-			                        .addHandlerLast(new WriteTimeoutHandler(5))
-			                )
-			        ));
+			            HttpClient.create(provider)));
 	}
 
 }
