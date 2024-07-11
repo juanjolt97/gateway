da127f8ce1b95f55fd721c72690d2584dd2c6e81
juan jose <gestion@starplast.es>
2024-07-11 19:10:43 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -1,9 +1,17 @@
 package com.service.gateway.config;
 
+import java.time.Duration;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -11,7 +19,16 @@ public class WebClientConfig {
 	@Bean
 	@LoadBalanced
 	public WebClient.Builder builder(){
-		return WebClient.builder();
+		return WebClient.builder()
+				.clientConnector(new ReactorClientHttpConnector(
+			            HttpClient.create()
+			                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+			                .responseTimeout(Duration.ofSeconds(5))
+			                .doOnConnected(conn -> 
+			                    conn.addHandlerLast(new ReadTimeoutHandler(5))
+			                        .addHandlerLast(new WriteTimeoutHandler(5))
+			                )
+			        ));
 	}
 
 }
