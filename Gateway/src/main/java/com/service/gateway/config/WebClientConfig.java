236081e17bd7831eee0036123e38389abb100d87
juan jose <gestion@starplast.es>
2024-07-16 16:37:53 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -8,10 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import io.netty.channel.ChannelOption;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
-import reactor.util.retry.Retry;
 
 @Configuration
 public class WebClientConfig {
@@ -21,21 +19,14 @@ public class WebClientConfig {
 	public WebClient.Builder builder(){
 		ConnectionProvider provider = ConnectionProvider.builder("fixed")
 			    .maxConnections(500)  
-			    .maxIdleTime(Duration.ofMinutes(30))  
-			    .maxLifeTime(Duration.ofHours(4)) 
+			    .maxIdleTime(Duration.ofSeconds(20))  
+			    .maxLifeTime(Duration.ofSeconds(60)) 
 			    .pendingAcquireTimeout(Duration.ofSeconds(60))  
 			    .evictInBackground(Duration.ofSeconds(120)) 
 			    .build();
 		
-		HttpClient httpClient = HttpClient.create(provider)
-			    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-			    .responseTimeout(Duration.ofSeconds(10))
-			    .keepAlive(true);
-		
 		return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .filter((request, next) -> next.exchange(request)
-                        .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(2))));
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider)));
 	}
 
 }
