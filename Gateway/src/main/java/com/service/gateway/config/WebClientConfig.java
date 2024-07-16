e70632a66500059fc238d4a51e3070394e8d8970
juan jose <gestion@starplast.es>
2024-07-16 18:43:45 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.service.gateway.config;
 
-import java.time.Duration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -9,24 +8,15 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class WebClientConfig {
 	
 	@Bean
 	@LoadBalanced
-	public WebClient.Builder builder(){
-		ConnectionProvider provider = ConnectionProvider.builder("fixed")
-			    .maxConnections(500)  
-			    .maxIdleTime(Duration.ofSeconds(20))  
-			    .maxLifeTime(Duration.ofSeconds(60)) 
-			    .pendingAcquireTimeout(Duration.ofSeconds(60))  
-			    .evictInBackground(Duration.ofSeconds(120)) 
-			    .build();
-		
+	public WebClient.Builder builder(){		
 		return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider)));
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()));
 	}
 
 }
