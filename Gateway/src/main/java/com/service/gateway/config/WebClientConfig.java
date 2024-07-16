3b53f22db6fe52b25f2112d82523471377521ac9
juan jose <gestion@starplast.es>
2024-07-16 07:29:14 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import io.netty.channel.ChannelOption;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
+import reactor.util.retry.Retry;
 
 @Configuration
 public class WebClientConfig {
@@ -28,10 +29,13 @@ public class WebClientConfig {
 		
 		HttpClient httpClient = HttpClient.create(provider)
 			    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-			    .responseTimeout(Duration.ofSeconds(10));
+			    .responseTimeout(Duration.ofSeconds(10))
+			    .keepAlive(true);
 		
 		return WebClient.builder()
-				.clientConnector(new ReactorClientHttpConnector(httpClient));
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .filter((request, next) -> next.exchange(request)
+                        .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(2))));
 	}
 
 }
