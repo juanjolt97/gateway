6ad3e08a9c9efda181a19a0cd041aa61189c63a9
juan jose <gestion@starplast.es>
2024-08-18 23:21:54 +0200

    fix

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -2,7 +2,6 @@ package com.service.gateway.config;
 
 
 
-import java.time.Duration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import io.netty.channel.ChannelOption;
-import io.netty.channel.epoll.EpollChannelOption;
-import io.netty.handler.logging.LogLevel;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
+
 
 @Configuration
 public class WebClientConfig {
@@ -23,18 +18,8 @@ public class WebClientConfig {
 	@Bean
 	@LoadBalanced
 	WebClient.Builder builder(){	
-		ConnectionProvider provider = ConnectionProvider.builder("fixed")
-                .maxConnections(500)
-                .maxIdleTime(Duration.ofSeconds(20))
-                .maxLifeTime(Duration.ofSeconds(60))
-                .pendingAcquireTimeout(Duration.ofSeconds(60))
-                .evictInBackground(Duration.ofSeconds(120)).build();
-
-        HttpClient httpClient = HttpClient.create(provider);
-        httpClient.warmup().block();
-
 		return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient));
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.newConnection()));
 	}
 
 }
