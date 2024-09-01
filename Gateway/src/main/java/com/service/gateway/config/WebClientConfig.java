018861504e748ac0b658dde5726f9f8e8a8ba3e7
juan jose <gestion@starplast.es>
2024-09-01 19:00:06 +0200

    fix properties

--- Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/service/gateway/config/WebClientConfig.java
@@ -6,10 +6,7 @@ package com.service.gateway.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import reactor.netty.http.client.HttpClient;
 
 
 @Configuration
@@ -18,8 +15,7 @@ public class WebClientConfig {
 	@Bean
 	@LoadBalanced
 	WebClient.Builder builder(){	
-		return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.newConnection()));
+		return WebClient.builder();
 	}
 
 }
